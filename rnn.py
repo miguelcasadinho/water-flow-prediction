d3a29d8eca7095989d36ec88282b4a960a3f4d00
miguelcasadinho <miguel.casadinho@gmail.com>
2024-10-06 16:05:07 +0100

    version 2024-10-06

--- rnn.py
+++ rnn.py
@@ -1,1 +1,18 @@
+# Recurrent Neural Network
 
+
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+# Read the csv with pandas
+dataset_train = pd.read_csv(('train_set.csv'), delimiter=';')
+# Leave only the flow column
+training_set = dataset_train.iloc[:, 3:4].values
+
+print(training_set)
